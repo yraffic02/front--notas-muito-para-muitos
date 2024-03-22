8c23b1acd9b5d852662d11d5046f5d42ff2eebca
yraffic02 <yraffic02@gmail.com>
2024-03-21 21:30:48 -0300

    chore: alterada tag main para o seo

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import { ListCards } from "@/components/ListCards";
 
 export default function Home() {
   return (
-    <div className="container mt-5">
+    <main className="container mt-5">
       <ListCards />
-    </div>
+    </main>
   );
 }
