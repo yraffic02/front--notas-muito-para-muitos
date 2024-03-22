71404552858e2e68ab467abdd3e6ff4f8ec52efb
yraffic02 <yraffic02@gmail.com>
2024-03-22 01:21:25 -0300

    chore: link para page home add

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,14 @@ import Link from "next/link"
 export const Header = ()=>{
     return(
         <header className="d-flex justify-content-between align-items-center mb-4 p-4">
-            <h1 className="m-0 fs-4">
-                Lista de Notas
-            </h1>
+            <Link
+                href='/'
+                className="text-decoration-none text-black "
+            >
+                <h1 className="m-0 fs-4">
+                    Lista de Notas
+                </h1>
+            </Link>
             <nav>
                 <Link href="/register/grades" className="me-3">
                     Criar notas
