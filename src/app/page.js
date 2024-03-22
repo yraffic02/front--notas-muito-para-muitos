4f4132e54588072a4a8db142b03df54f8a1d347a
yraffic02 <yraffic02@gmail.com>
2024-03-22 13:36:18 -0300

    chore: add link

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import { ListCardsNotas } from "./components/ListCardsNotas";
 
 
 export default function Home() {
   return (
     <main className="container mt-5">
+      <Link
+        href='/tags'
+      >
+        Ver lista de tags
+      </Link>
       <ListCardsNotas />
     </main>
   );
--- src/app/tags/page.js
+++ src/app/tags/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import { ListCardsTags } from "./components/ListCardsTags";
 
 
 export default function Tags() {
   return (
     <main className="container mt-5">
+      <Link
+        href='/'
+      >
+        Ver lista de notas
+      </Link>
       <ListCardsTags />
     </main>
   );
