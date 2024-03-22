50f437f54b7124f0559a21d73eafce8f4c59a7cd
yraffic02 <yraffic02@gmail.com>
2024-03-21 21:05:48 -0300

    chore: retirado texto da page

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,6 @@ import { ListCards } from "@/components/ListCards";
 export default function Home() {
   return (
     <div className="container mt-5">
-      <h1 className="mb-4 ">
-        Lista de Notas
-      </h1>
       <ListCards />
     </div>
   );
