0c581e93f9cf9ddf2a9db2912af60de9144e8cc1
yraffic02 <yraffic02@gmail.com>
2024-03-22 20:15:37 -0300

    fix: removido search

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,9 @@
 'use client'
 import Link from "next/link";
 import { ListCardsNotas } from "./components/ListCardsNotas";
-import { Search } from "@/components/Search";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getTags } from "@/redux/features/tags-slice";
 
 
 export default function Home() {
-  const tags = useSelector((state)=>  state.tagsReducer.value)
-  const dispatch = useDispatch()
-
-
-  useEffect(()=>{
-    dispatch(getTags())
-  }, [])
-
   return (
     <main className="container mt-5">
       <div className="d-flex justify-content-between p-3 ">
@@ -24,8 +12,6 @@ export default function Home() {
         >
           Ver lista de tags
         </Link>
-
-        <Search tags={tags}/>
       </div>
       <ListCardsNotas />
     </main>
