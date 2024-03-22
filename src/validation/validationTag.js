55b83d382c7bc833604afdf80c7e229bbbd34b11
yraffic02 <yraffic02@gmail.com>
2024-03-22 00:16:11 -0300

    fix: nome corrigido

--- src/validation/validationTag.js
+++ src/validation/validationTag.js
@@ -1,6 +1,6 @@
 const yup = require('yup')
 
-export const schemaNota = yup.object({
+export const schemaTag = yup.object({
     titulo: yup.string().required("O título é obrigatório"),
     notas: yup.array().of(yup.number())
 });
