a9a33e29bce442fab49baecf3c3b1f8fa134e65d
yraffic02 <yraffic02@gmail.com>
2024-03-21 21:52:21 -0300

    fix: correção de class button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ export const TypeButton = {
 };
 
 export const Button = ({ typeButton, children, ...props }) =>{   
-    const buttonClass = typeButton === typeButton.PRIMARY ? 'btn btn-primary me-2' : 'btn btn-secondary';
+    const buttonClass = typeButton === TypeButton.PRIMARY ? 'btn btn-primary me-2' : 'btn btn-danger';
           
     return (
         <button className={buttonClass} {...props}>
