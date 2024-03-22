1b28e95adbb25cf57e771ff5d4985f397dbd1bf0
yraffic02 <yraffic02@gmail.com>
2024-03-22 14:52:31 -0300

    chore: criado action filter

--- src/redux/features/notas-slice.js
+++ src/redux/features/notas-slice.js
@@ -17,6 +17,15 @@ export const getNotas = createAsyncThunk('nota/getNota', async (thunkAPI) => {
   }
 });
 
+export const getNotasByTagId = createAsyncThunk('nota/getNotaByTagId', async (tagId, thunkAPI) => {
+  try {
+    const response = await api.get(`/notas?tagId=${tagId}`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data); 
+  }
+});
+
 export const notasSlice = createSlice({
   name: 'notas',
   initialState,
@@ -32,6 +41,18 @@ export const notasSlice = createSlice({
         state.error = null;
       })
       .addCase(getNotas.rejected, (state, action) => {
+        state.status = 'idle';
+        state.error = action.payload;
+      })
+      .addCase(getNotasByTagId.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getNotasByTagId.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.value = action.payload;
+        state.error = null;
+      })
+      .addCase(getNotasByTagId.rejected, (state, action) => {
         state.status = 'idle';
         state.error = action.payload;
       });
